34600ee965db82d5457b7fdd970db559e25ec6e6
MaazouzMehdi <skyrim1995@hotmail.fr>
2022-03-09 18:40:12 +0100

    add new version of workshop and fix bugs

--- Code/format.sh
+++ Code/format.sh
@@ -1,9 +1,9 @@
-sed  -i '16s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '17s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '22s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '23s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '43s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '44s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '49s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
-sed  -i '50s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./routes.sql
+sed  -i '16s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '17s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '22s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '23s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '43s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '44s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '49s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
+sed  -i '50s/NUMERIC(5,8);/NUMERIC(5,0);/g' ./Code/routes.sql
 
